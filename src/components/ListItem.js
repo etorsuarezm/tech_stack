4be918ec4989ca6339fe6d442b4b404aa8fd1122
Hector Suarez <etorsuarez@gmail.com>
2017-02-03 09:35:35 -0600

    Move expanded logic outside of the component and added basic Layout Animation

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { 
+    Text, 
+    TouchableWithoutFeedback, 
+    View,
+    LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
+
     renderDescription() {
-        const { library, selectedLibraryId } = this.props; 
-        if (library.id === selectedLibraryId) {
+        const { library, expanded } = this.props; 
+        if (expanded) {
             return (
-                <Text>
-                    { library.description }
-                </Text>
+                <CardSection>
+                    <Text style={styles.descriptionStyle}>
+                        { library.description }
+                    </Text>
+                </CardSection>
             );
         }
     }
 
-
     render() {
-        const { titleStyle } = styles;
         const { id, title } = this.props.library;
 
         return (
@@ -26,8 +35,8 @@ class ListItem extends Component {
                 onPress={() => this.props.selectLibrary(id)}
             >
                 <View>
-                    <CardSection>
-                        <Text style={titleStyle} >
+                    <CardSection >
+                        <Text style={styles.titleStyle} >
                             {title}
                         </Text>
                     </CardSection>
@@ -42,11 +51,18 @@ const styles = {
     titleStyle: {
         fontSize: 18,
         paddingLeft: 15
+    },
+    descriptionStyle: {
+        flex: 1, 
+        paddingLeft: 5, 
+        paddingRight: 5
     }
 };
 
-const mapStateToProps = state => {
-    return { selectedLibraryId: state.selectedLibraryId };
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+
+    return { expanded };
 };
 
 export default connect(mapStateToProps, actions)(ListItem);
